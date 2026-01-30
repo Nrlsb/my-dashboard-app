a46022a692066313071ef5f7d534be5e4b2292dc
Nrlsb <luksbenitez01@hotmail.com>
2026-01-30 12:43:25 -0300

    Actualizacion 3.0.0756

--- src/api/services/cart.service.js
+++ src/api/services/cart.service.js
@@ -3,11 +3,11 @@ import apiClient from '../core/client';
 export const cartService = {
     getCart: async () => {
         const response = await apiClient.get('/cart');
-        return response.data;
+        return response; // Response is already data due to interceptor
     },
 
     updateCart: async (items) => {
         const response = await apiClient.post('/cart', { items });
-        return response.data;
+        return response; // Response is already data due to interceptor
     }
 };
